5fb8b71be1092a1486939b0d4d93892cdb7e5b30
Nikita <32540777+nivanown@users.noreply.github.com>
2023-01-05 09:15:58 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+﻿/*- new-products-slider -*/
+const swiper = new Swiper(".news-slider", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".slider-arrow .swiper-button-next",
+    prevEl: ".slider-arrow .swiper-button-prev",
+  },
+});
+
+/*- tabs -*/
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
