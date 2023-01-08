6756570efbaa11d90066c26aee4ffcff5cc2d10b
Nikita <32540777+nivanown@users.noreply.github.com>
2023-01-08 10:06:32 +0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,14 @@ const swiper = new Swiper(".news-slider", {
     nextEl: ".slider-arrow .swiper-button-next",
     prevEl: ".slider-arrow .swiper-button-prev",
   },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    540: {
+      slidesPerView: 2,
+    },
+  },
 });
 
 /*- tabs -*/
@@ -24,5 +32,22 @@ tabs.forEach(tab => {
     })
     tab.classList.add('active')
     target.classList.add('active')
+  })
+})
+
+/*- mobile menu -*/
+let toggleButton = document.querySelector('.header__mob-btn');
+let navBar = document.querySelector('.header__bottom-panel');
+toggleButton.addEventListener('click', function () {
+  toggleButton.classList.toggle('open');
+  navBar.classList.toggle('open');
+});
+
+/*- main-navi -*/
+const iconUpDown = document.querySelectorAll(".main-navi_down-arrow a");
+iconUpDown.forEach(link => {
+  link.addEventListener("click", e => {
+    link.classList.toggle("active")
+    link.parentElement.classList.toggle("open");
   })
 })
